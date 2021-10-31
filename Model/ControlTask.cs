46c71328aa23143c8b9717ad98fd742a444f5ab2
welter <welter@163.com>
2021-10-31 22:04:41 +0800

    增加controlTask

---
+++ Model/ControlTask.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfTimeController.Model
+{
+    enum RunningRule { };
+    enum LimitRule { };
+    class ControlTask : NotificationObject
+    {
+        public UInt16 Id { get; set; }
+        public string TaskName { get; set; }
+        public string ProgramTitle { get; set; }
+        public string ProgramDirectory { get; set; }
+        public string RunPath { get; set; }
+        public RunningRule Rule { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime PerPeriodTime { get; set; }
+        public int Times { get; set; }
+        public DateTime TotalTime { get; set; }
+        public LimitRule Limit { get; set; }
+
+    }
+}
