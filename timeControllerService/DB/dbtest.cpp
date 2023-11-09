4a8cc558aefefed5527976afe2967231be6c1c4b
welter <welter@163.com>
2023-11-10 01:25:38 +0800

    增加GenRsaPubEncrypt函数

--- timeControllerService/DB/dbtest.cpp
+++ timeControllerService/DB/dbtest.cpp
@@ -15,6 +15,10 @@
 #include <zip.h>
 #include <tchar.h>
 #include "../Timer/pscmd.h"
+#include <fstream>
+#include "openssl/rsa.h"
+#include <openssl/pem.h>
+
 //#include <vector>
 using namespace std;
 
@@ -133,6 +137,51 @@ static WindowsTimer timerMoniteTimer, timerlogTimer;
 
 void InitService();//前置声明
 
+/*
+@brief : 公钥加密
+@para  : clear_text  -[i] 需要进行加密的明文
+		 pub_key     -[i] 公钥
+@return: 加密后的数据
+**/
+std::string GenRsaPubEncrypt(const std::string& clear_text, const std::string& pub_key)
+{
+	std::string encrypt_text;
+	BIO* keybio = BIO_new_mem_buf((unsigned char*)pub_key.c_str(), -1);
+	RSA* rsa = RSA_new();
+	// 注意-----第1种格式的公钥
+	//rsa = PEM_read_bio_RSAPublicKey(keybio, &rsa, NULL, NULL);
+	// 注意-----第2种格式的公钥（这里以第二种格式为例）
+	rsa = PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
+
+	// 获取RSA单次可以处理的数据块的最大长度
+	int key_len = RSA_size(rsa);
+	int block_len = key_len - 11;    // 因为填充方式为RSA_PKCS1_PADDING, 所以要在key_len基础上减去11
+
+	// 申请内存：存贮加密后的密文数据
+	char* sub_text = new char[key_len + 1];
+	memset(sub_text, 0, key_len + 1);
+	int ret = 0;
+	int pos = 0;
+	std::string sub_str;
+	// 对数据进行分段加密（返回值是加密后数据的长度）
+	while (pos < clear_text.length()) {
+		sub_str = clear_text.substr(pos, block_len);
+		memset(sub_text, 0, key_len + 1);
+		ret = RSA_public_encrypt(sub_str.length(), (const unsigned char*)sub_str.c_str(), (unsigned char*)sub_text, rsa, RSA_PKCS1_PADDING);
+		if (ret >= 0) {
+			encrypt_text.append(std::string(sub_text, ret));
+		}
+		pos += block_len;
+	}
+
+	// 释放内存  
+	BIO_free_all(keybio);
+	RSA_free(rsa);
+	delete[] sub_text;
+
+	return encrypt_text;
+}
+
 //以进程名在监控队列中查找进程
 //参数：proceName，进程名
 //返回：找到的进程在监控队列中指针，若没找到返回空指针。
@@ -153,42 +202,42 @@ BOOL ValidateToken(char* token)
 	return true;
 }
 
-BOOL ValidateUser(char* userName,char * passWord,char* token)
+BOOL ValidateUser(string userName,string passWord,string& token)
 {
 	const char* DBPath = "user.db";
-//	SQLite::Database* db;
+	SQLite::Database* db;
 	try {
-//		db = new SQLite::Database(DBPath, SQLite::OPEN_READWRITE);
+		db = new SQLite::Database(DBPath, SQLite::OPEN_READWRITE);
 	}
 	catch (std::exception& e)
 	{
 		//ConsolePrintf("exception: %s\n", e.what()); ConsoleScanf(ch, len);
 		try {
-//			db = new SQLite::Database(DBPath, SQLite::OPEN_CREATE | SQLite::OPEN_READWRITE);
+			db = new SQLite::Database(DBPath, SQLite::OPEN_CREATE | SQLite::OPEN_READWRITE);
 		}
 		catch (std::exception& e) {
 			return false;
 		}
 	}
 
-//	if (!(*db).tableExists("USER"))
+	if (!(*db).tableExists("USER"))
 	{
 		const char* sql;
 		sql = "select * from user where username from USER";
-//		SQLite::Statement mQuery((*db), sql);
-//		mQuery.bind(":USERNAME", userName);
-//		if (mQuery.executeStep())
-//			(*db).exec(sql);
-//		char* pw = (char*)mQuery.getColumn(2).getText();
-//		if (strcmp(passWord, pw) == 0)
+		SQLite::Statement mQuery((*db), sql);
+		mQuery.bind(":USERNAME", userName);
+		if (mQuery.executeStep())
+			(*db).exec(sql);
+		char* pw = (char*)mQuery.getColumn(2).getText();
+		if (passWord!=pw)
 		{
-			scanf(token, "asfd");
-//			(*db).~Database();
+			token=GenRsaPubEncrypt(userName, "asdfsdf");
+			(*db).~Database();
 			return true;
 		}
 	}
 	try {
-//		(*db).~Database();
+		(*db).~Database();
 		return false;
 	}
 	catch (std::exception& e)
